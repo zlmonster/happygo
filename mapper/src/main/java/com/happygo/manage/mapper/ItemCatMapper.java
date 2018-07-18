124b55910e633cd910973516417905d6ca7a6bc7
DragonJackieLee <dragonjackielee@163.com>
2018-07-18 14:31:51 +0800

    test

--- mapper/src/main/java/com/happygo/manage/mapper/ItemCatMapper.java
+++ mapper/src/main/java/com/happygo/manage/mapper/ItemCatMapper.java
@@ -4,7 +4,7 @@ import com.github.abel533.mapper.Mapper;
 import com.happygo.manage.pojo.ItemCat;
 
 /**
- * @author <a href="mailto:wb-lzl282164@alibaba-inc.com">李智龙</a>
+ * @author <a href="mailto:wb-lzl282164@alibaba-inc.com">李智龙.</a>
  * @since 2018/7/16
  */
 public interface ItemCatMapper extends Mapper<ItemCat> {
