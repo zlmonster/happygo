33db30ce0b4207c740140af2efd5461672bb6b5b
DragonJackieLee <dragonjackielee@163.com>
2018-07-26 15:12:59 +0800

    refactor project

--- mapper/src/main/java/com/happygo/manage/mapper/ItemParamMapper.java
+++
@@ -1,8 +0,0 @@
-package com.happygo.manage.mapper;
-
-import com.github.abel533.mapper.Mapper;
-import com.happygo.manage.pojo.ItemParam;
-
-public interface ItemParamMapper extends Mapper<ItemParam>{
-
-}
